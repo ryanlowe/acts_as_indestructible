62743939b85922e7ce478a904368f6af5e0f3d1b
Ryan Lowe <ryanlowe@gmail.com>
2008-05-26 13:26:41 -0400

    test destroy_all with conditions

--- test/unit/class/destroy_all_test.rb
+++ test/unit/class/destroy_all_test.rb
@@ -10,35 +10,59 @@ class DestroyAllTest < Test::Unit::TestCase
   def test_destroy_all
     assert  indestructible_posts(:destroyed).destroyed?
     assert !indestructible_posts(:hello).destroyed?
+    assert !indestructible_posts(:goodbye).destroyed?
+    assert !indestructible_posts(:no_body).destroyed?
     
     IndestructiblePost.destroy_all(indestructible_users(:ryanlowe))
     
     assert  indestructible_posts(:destroyed).reload.destroyed?
     assert  indestructible_posts(:hello).reload.destroyed?
+    assert  indestructible_posts(:goodbye).reload.destroyed?
+    assert  indestructible_posts(:no_body).reload.destroyed?
   end
   
   def test_destroy_all_with_conditions
-    #TODO:
+    assert  indestructible_posts(:destroyed).destroyed?
+    assert !indestructible_posts(:hello).destroyed?
+    assert !indestructible_posts(:goodbye).destroyed?
+    assert !indestructible_posts(:no_body).destroyed?
+    
+    assert_equal 'Hello World!', indestructible_posts(:hello).title
+    
+    IndestructiblePost.destroy_all(indestructible_users(:ryanlowe),"title = 'Hello World!'")
+    
+    assert  indestructible_posts(:destroyed).reload.destroyed?
+    assert  indestructible_posts(:hello).reload.destroyed?
+    assert !indestructible_posts(:goodbye).reload.destroyed?
+    assert !indestructible_posts(:no_body).reload.destroyed?
   end
   
   def test_destroy_all_nil_user
     assert  indestructible_posts(:destroyed).destroyed?
     assert !indestructible_posts(:hello).destroyed?
+    assert !indestructible_posts(:goodbye).destroyed?
+    assert !indestructible_posts(:no_body).destroyed?
     
     IndestructiblePost.destroy_all(nil)
     
     assert  indestructible_posts(:destroyed).reload.destroyed?
     assert !indestructible_posts(:hello).reload.destroyed?
+    assert !indestructible_posts(:goodbye).reload.destroyed?
+    assert !indestructible_posts(:no_body).reload.destroyed?
   end
   
   def test_destroy_all_not_user
     assert  indestructible_posts(:destroyed).destroyed?
     assert !indestructible_posts(:hello).destroyed?
+    assert !indestructible_posts(:goodbye).destroyed?
+    assert !indestructible_posts(:no_body).destroyed?
     
     IndestructiblePost.destroy_all(Object.new)
     
     assert  indestructible_posts(:destroyed).reload.destroyed?
     assert !indestructible_posts(:hello).reload.destroyed?
+    assert !indestructible_posts(:goodbye).reload.destroyed?
+    assert !indestructible_posts(:no_body).reload.destroyed?
   end
   
 end
